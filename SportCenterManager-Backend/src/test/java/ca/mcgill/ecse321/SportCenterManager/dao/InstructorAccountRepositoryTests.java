cdd132d73b74b4154db835bbf9b7988571053b89
Mahmoud1890 <143809939+Mahmoud1890@users.noreply.github.com>
2024-02-16 22:43:54 -0500

    Finished Instructor Account Persistence

--- SportCenterManager-Backend/src/test/java/ca/mcgill/ecse321/SportCenterManager/dao/InstructorAccountRepositoryTests.java
+++ SportCenterManager-Backend/src/test/java/ca/mcgill/ecse321/SportCenterManager/dao/InstructorAccountRepositoryTests.java
@@ -1,4 +1,39 @@
 package ca.mcgill.ecse321.SportCenterManager.dao;
 
+import ca.mcgill.ecse321.SportCenterManager.model.InstructorAccount;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
 public class InstructorAccountRepositoryTests {
+    @Autowired InstructorAccountRepository instructorAccountRepository;
+
+    @AfterEach
+    public void clearDatabase() {
+        instructorAccountRepository.deleteAll();
+    }
+
+    @Test
+    public void testPersistanceAndLoadInstructorAccount(){
+        String name = "Mahmoud";
+        String email = "mahmoud@gmail.com" ;
+        String password = "abcdefg";
+
+        InstructorAccount instructorAccount= new InstructorAccount(name, email, password);
+        instructorAccount=instructorAccountRepository.save(instructorAccount);
+        int accountID = instructorAccount.getId();
+
+        InstructorAccount instructorAccountDatabase = instructorAccountRepository.findInstructorAccountById(accountID);
+
+        assertNotNull(instructorAccountDatabase);
+        assertEquals(name, instructorAccountDatabase.getName());
+        assertEquals(email, instructorAccountDatabase.getEmail());
+        assertEquals(password, instructorAccountDatabase.getPassword());
+    }
+
 }
