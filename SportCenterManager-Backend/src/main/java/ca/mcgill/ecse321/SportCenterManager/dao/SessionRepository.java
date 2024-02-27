cab17e491a8f9fefb1934fda54d50be6baf88713
angelaxzhu <angela.x.zhu@mail.mcgill.ca>
2024-02-27 17:54:45 -0500

    added sessions methods to events service & added method to session repo for delete by foreign key of course

--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/dao/SessionRepository.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/dao/SessionRepository.java
@@ -4,4 +4,5 @@ import ca.mcgill.ecse321.SportCenterManager.model.Session;
 
 public interface SessionRepository extends CrudRepository<Session, Integer> {
   public Session findSessionById(int id);
+  public void deleteByCourse(int course_id);
 }
--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/service/EventService.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/service/EventService.java
@@ -1,16 +1,31 @@
 package ca.mcgill.ecse321.SportCenterManager.service;
 
 import ca.mcgill.ecse321.SportCenterManager.dao.CourseRepository;
+import ca.mcgill.ecse321.SportCenterManager.dao.SessionRepository;
 import ca.mcgill.ecse321.SportCenterManager.model.Course;
+import ca.mcgill.ecse321.SportCenterManager.model.Session;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters.LocalDateConverter;
+
 import jakarta.transaction.Transactional;
 
 @Service
 public class EventService {
     @Autowired
     private CourseRepository courseRepo;
+    @Autowired
+    private SessionRepository sessionRepo;
 
     @Transactional 
     public Iterable<Course> findAllCourses(){
@@ -39,4 +54,40 @@ public class EventService {
         Course courseToCreate = new Course(name, description, costPerSession);
 	    return courseRepo.save(courseToCreate); 
     }
+
+    //sessions
+
+    @Transactional
+    public Iterable<Session> findAllSessionsOfCourse(int course_id){
+			return sessionRepo.findAll();
+		}
+    
+    @Transactional
+    public Session findSessionById(int session_id){
+			return sessionRepo.findSessionById(session_id);
+		}
+    
+    @Transactional
+    public void deleteAllSessionsOfCourse(int course_id){
+			//need to find all sessions that have foreign key course_id
+            sessionRepo.deleteByCourse(course_id); //added a method in repository...
+		}
+
+    @Transactional
+    public void deleteSessionById(int session_id){
+			//return 
+            sessionRepo.deleteById(session_id);
+		}
+
+    @Transactional
+    public void modifySessionById(){
+			//return 
+		}
+    
+    //@Transactional
+    //public Session createSession(LocalTime start_time, LocalTime end_time, LocalDate date){
+        //Time and Date in model, change? 
+        //Session sessionToCreate = new Session(start_time, end_time, date);
+	    //return sessionRepo.save(sessionToCreate); 
+	//}
 }
