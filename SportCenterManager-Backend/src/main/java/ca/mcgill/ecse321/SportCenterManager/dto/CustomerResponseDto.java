ec3ccdc6a3234372788b19a868b79e63f71da67d
jennifertramsu <jennifer.tramsu@gmail.com>
2024-02-27 15:25:37 -0500

    customer and instructor DTOs

--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/dto/CustomerResponseDto.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/dto/CustomerResponseDto.java
@@ -1,5 +1,43 @@
 package ca.mcgill.ecse321.SportCenterManager.dto;
 
+import ca.mcgill.ecse321.SportCenterManager.model.CustomerAccount;
+
 public class CustomerResponseDto {
-    
+    private int id;
+    private String name;
+    private String email;
+
+    public CustomerResponseDto(CustomerAccount model) {
+        this.id = model.getId();
+        this.name = model.getName();
+        this.email = model.getEmail();
+    }
+
+    // Getters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/dto/InstructorListDto.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/dto/InstructorListDto.java
@@ -1,5 +1,19 @@
 package ca.mcgill.ecse321.SportCenterManager.dto;
 
+import java.util.List;
+
 public class InstructorListDto {
-    
+    private List<InstructorResponseDto> instructors;
+
+    public InstructorListDto(List<InstructorResponseDto> instructors) {
+        this.instructors = instructors;
+    }
+
+    public List<InstructorResponseDto> getInstructors() {
+        return instructors;
+    }
+
+    public void setInstructors(List<InstructorResponseDto> instructors) {
+        this.instructors = instructors;
+    }
 }
