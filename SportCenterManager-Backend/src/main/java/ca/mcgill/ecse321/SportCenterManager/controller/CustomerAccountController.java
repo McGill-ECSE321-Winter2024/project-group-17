16aaec774f0c4712c8f0efc261a18a54d0ebfac9
jennifertramsu <jennifer.tramsu@gmail.com>
2024-02-27 14:57:21 -0500

    modifying customer controller

--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/controller/CustomerAccountController.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/controller/CustomerAccountController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ca.mcgill.ecse321.SportCenterManager.dao.CustomerAccountRepository;
-import ca.mcgill.ecse321.SportCenterManager.model.InstructorAccount;
+import ca.mcgill.ecse321.SportCenterManager.dao.InstructorAccountRepository;
 
 @Service
 public class CustomerAccountController {
