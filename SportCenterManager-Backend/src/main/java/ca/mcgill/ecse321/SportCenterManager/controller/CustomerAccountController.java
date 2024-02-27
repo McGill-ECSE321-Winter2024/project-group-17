04ef558373d23e5a4da80083d67fd7d2420db205
jennifertramsu <jennifer.tramsu@gmail.com>
2024-02-27 16:13:49 -0500

    customer controller and service

--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/controller/CustomerAccountController.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/controller/CustomerAccountController.java
@@ -1,17 +1,55 @@
 package ca.mcgill.ecse321.SportCenterManager.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
-import ca.mcgill.ecse321.SportCenterManager.dao.CustomerAccountRepository;
-import ca.mcgill.ecse321.SportCenterManager.dao.InstructorAccountRepository;
+import ca.mcgill.ecse321.SportCenterManager.dto.CustomerListDto;
+import ca.mcgill.ecse321.SportCenterManager.dto.CustomerRequestDto;
+import ca.mcgill.ecse321.SportCenterManager.dto.CustomerResponseDto;
+import ca.mcgill.ecse321.SportCenterManager.model.CustomerAccount;
+import ca.mcgill.ecse321.SportCenterManager.service.CustomerAccountService;
 
-@Service
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
 public class CustomerAccountController {
     @Autowired
-    private CustomerAccountRepository customerRepo;
-    @Autowired
-    private InstructorAccountRepository instructorRepo;
-    
-    
+    private CustomerAccountService customerAccountService;
+
+    /*
+     * PUT /customerAccounts/{customerAccount_id}
+     */
+
+    @GetMapping("/customerAccounts/{customerAccount_id}")
+    public CustomerResponseDto findCustomerById(@PathVariable int customerAccount_id) {
+        CustomerAccount customer = customerAccountService.findCustomerById(customerAccount_id);
+        return new CustomerResponseDto(customer);
+    }
+
+    @GetMapping("/customerAccounts")
+    public CustomerListDto findAllCustomers() {
+        List<CustomerResponseDto> customers = new ArrayList<CustomerResponseDto>();
+        for (CustomerAccount customer : customerAccountService.findAllCustomers()) {
+            customers.add(new CustomerResponseDto(customer));
+        }
+        return new CustomerListDto(customers);
+    }
+
+    @PostMapping("/customerAccounts")
+    public CustomerResponseDto createCustomer(@RequestBody CustomerRequestDto customer) {
+        CustomerAccount customerToCreate = customerAccountService.createCustomer(customer.getName(), customer.getEmail(), customer.getPassword());
+        return new CustomerResponseDto(customerToCreate);
+    }   
+
+    @DeleteMapping("/customerAccounts/{customerAccount_id}")
+    public void deleteCustomer(@PathVariable int customerAccount_id) {
+        customerAccountService.deleteCustomer(customerAccount_id);
+    }
 }
--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/service/CustomerAccountService.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/service/CustomerAccountService.java
@@ -1,5 +1,39 @@
 package ca.mcgill.ecse321.SportCenterManager.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ca.mcgill.ecse321.SportCenterManager.dao.CustomerAccountRepository;
+import ca.mcgill.ecse321.SportCenterManager.model.CustomerAccount;
+import jakarta.transaction.Transactional;
+
+@Service
 public class CustomerAccountService {
-    
+    @Autowired
+    private CustomerAccountRepository customerRepo;
+
+    /*
+     * Modify Account
+     */
+
+     @Transactional
+     public Iterable<CustomerAccount> findAllCustomers() {
+        return customerRepo.findAll();
+     }
+
+     @Transactional
+     public CustomerAccount findCustomerById(int id) {
+        return customerRepo.findCustomerAccountById(id);
+     }
+
+     @Transactional
+     public CustomerAccount createCustomer(String name, String email, String password) {
+        CustomerAccount customerToCreate = new CustomerAccount(name, email, password);
+        return customerRepo.save(customerToCreate);
+     }
+
+     @Transactional
+     public void deleteCustomer(int id) {
+        customerRepo.deleteById(id);
+     }
 }
