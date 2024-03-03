f2561862e28a01acdc68775948f1c6c8e882fea5
jennifertramsu <jennifer.tramsu@gmail.com>
2024-03-03 10:49:00 -0500

    adding more methods to customer dao

--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/dao/CustomerAccountRepository.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/dao/CustomerAccountRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerAccountRepository extends CrudRepository<CustomerAccount, Integer> {
   public CustomerAccount findCustomerAccountById(int id);
+  public CustomerAccount findCustomerAccountByEmail(String email);
+  public CustomerAccount findCustomerAccountByEmailAndPassword(String email, String password);
+  public boolean existsCustomerAccountByEmail(String email);
+  public boolean existsCustomerAccountByEmailAndPassword(String email, String password);
 }
