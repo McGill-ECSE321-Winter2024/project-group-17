126e3b48d6c9cc9a05e410cfe9338a3a8552521b
eric_zhu <eric.zhu@mail.mcgill.ca>
2024-03-07 00:11:08 -0500

    Refactored return value of getRegistrants

--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/controller/RegistrationController.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/controller/RegistrationController.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.SportCenterManager.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ca.mcgill.ecse321.SportCenterManager.dto.CustomerListDto;
+import ca.mcgill.ecse321.SportCenterManager.dto.CustomerResponseDto;
 import ca.mcgill.ecse321.SportCenterManager.dto.RegistrationListDto;
 import ca.mcgill.ecse321.SportCenterManager.dto.RegistrationResponseDto;
+import ca.mcgill.ecse321.SportCenterManager.model.CustomerAccount;
 import ca.mcgill.ecse321.SportCenterManager.model.Registration;
 import ca.mcgill.ecse321.SportCenterManager.service.RegistrationService;
 
@@ -33,10 +37,13 @@ public class RegistrationController {
     	return new RegistrationListDto(registrations);
     }
     
-    @GetMapping("/registrations/sessions/{sessionId}")
-    public RegistrationListDto findAllRegistrationsBySession(@PathVariable int sessionId) {
-    	List<Registration> registrations = service.findSessionRegistrations(sessionId);
-    	return new RegistrationListDto(registrations);
+    @GetMapping("/registrations/{sessionId}/customers")
+    public CustomerListDto findAllRegistrantsForSession(@PathVariable int sessionId) {
+    	List<CustomerResponseDto> registrants = new ArrayList<CustomerResponseDto>();
+    	for (CustomerAccount customer : service.findSessionRegistrants(sessionId)) {
+    		registrants.add(new CustomerResponseDto(customer));
+    	}
+    	return new CustomerListDto(registrants);
     }
     
     @DeleteMapping("/registrations/{customerId}/{sessionId}")
