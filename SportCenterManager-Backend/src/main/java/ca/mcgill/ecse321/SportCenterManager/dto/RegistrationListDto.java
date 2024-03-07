c48416ed7ea85ebbd10a14d9d53838700f0ba672
eric_zhu <eric.zhu@mail.mcgill.ca>
2024-03-07 15:56:32 -0500

    Implemented Integration Tests for FindRegistrationsByCustomer

--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/dto/RegistrationListDto.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/dto/RegistrationListDto.java
@@ -1,11 +1,17 @@
 package ca.mcgill.ecse321.SportCenterManager.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ca.mcgill.ecse321.SportCenterManager.model.Registration;
 
 public class RegistrationListDto {
-    private List<RegistrationResponseDto> registrations;
+    private List<RegistrationResponseDto> registrations = new ArrayList<RegistrationResponseDto>();
+    
+    @SuppressWarnings("unused")
+    public RegistrationListDto() {
+    	
+    }
     
     public RegistrationListDto(List<Registration> registrations) {
     	for (Registration registration: registrations) {
--- SportCenterManager-Backend/src/test/java/ca/mcgill/ecse321/SportCenterManager/integration/RegistrationIntegrationTests.java
+++ SportCenterManager-Backend/src/test/java/ca/mcgill/ecse321/SportCenterManager/integration/RegistrationIntegrationTests.java
@@ -2,12 +2,15 @@ package ca.mcgill.ecse321.SportCenterManager.integration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +26,8 @@ import ca.mcgill.ecse321.SportCenterManager.dao.InstructorAccountRepository;
 import ca.mcgill.ecse321.SportCenterManager.dao.RegistrationRepository;
 import ca.mcgill.ecse321.SportCenterManager.dao.ScheduleRepository;
 import ca.mcgill.ecse321.SportCenterManager.dao.SessionRepository;
+import ca.mcgill.ecse321.SportCenterManager.dto.ErrorDto;
+import ca.mcgill.ecse321.SportCenterManager.dto.RegistrationListDto;
 import ca.mcgill.ecse321.SportCenterManager.dto.RegistrationResponseDto;
 import ca.mcgill.ecse321.SportCenterManager.model.Course;
 import ca.mcgill.ecse321.SportCenterManager.model.CustomerAccount;
@@ -47,7 +52,8 @@ public class RegistrationIntegrationTests {
 	@Autowired
 	private ScheduleRepository scheduleRepo;
 	
-	private int customerId;
+	private int customerOneId;
+	private int customerTwoId;
 	private int sessionOneId;
 	private int sessionTwoId;
 	private int sessionThreeId;
@@ -61,7 +67,7 @@ public class RegistrationIntegrationTests {
 	
 	@AfterEach
 	public void cleanDatabase() {
-//		clearDatabase();
+		clearDatabase();
 	}
 	
 	private void clearDatabase() {
@@ -76,9 +82,9 @@ public class RegistrationIntegrationTests {
 	private void populateDatabase() {
 		// Prefills the database with information that would've been in there at the point of registration
 		CustomerAccount customer = new CustomerAccount("Tibo", "tibo123@gmail.com", "password#123");
-		this.customerId = customerRepo.save(customer).getId();
+		this.customerOneId = customerRepo.save(customer).getId();
 		CustomerAccount otherCustomer = new CustomerAccount("Mahmoud", "MahmoudA2@gmail.com", "smolPassword2@");
-		customerRepo.save(otherCustomer);
+		this.customerTwoId = customerRepo.save(otherCustomer).getId();
 		
 		InstructorAccount instructorOne = new InstructorAccount("Haoyuan", "5g@gmail.com", "bigPassword4@");
 		instructorRepo.save(instructorOne);
@@ -110,14 +116,75 @@ public class RegistrationIntegrationTests {
 	}
 	
 	@Test
-	public void testRegister() {
-		ResponseEntity<RegistrationResponseDto> response = client.exchange("/registrations/" + Integer.toString(customerId) + "/" + Integer.toString(sessionOneId), 
+	@Order(1)
+	public void testRegisterValid() {
+		registerCustomerValid(customerOneId, sessionOneId);
+	}
+	
+
+	
+	@Test
+	@Order(2)
+	public void testRegisterWithDuplicate() {
+		registerCustomerValid(customerOneId, sessionOneId);
+		ResponseEntity<ErrorDto> response = client.exchange("/registrations/" + Integer.toString(customerOneId) + "/" + Integer.toString(sessionOneId), 
+				HttpMethod.PUT, null, ErrorDto.class);
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+		assertEquals("Failed to Register: You are already registered to this session!", response.getBody().getMessage());
+	}
+	
+	@Test
+	@Order(3)
+	public void testRegisterWithConflict() {
+		registerCustomerValid(customerOneId, sessionOneId);
+		ResponseEntity<ErrorDto> response = client.exchange("/registrations/" + Integer.toString(customerOneId) + "/" + Integer.toString(sessionFourId), 
+				HttpMethod.PUT, null, ErrorDto.class);
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+		assertEquals("Failed to Register: Session overlaps with an existing registration!", response.getBody().getMessage());
+	}
+	
+	@Test
+	@Order(4)
+	public void findAllRegistrationsByCustomer() {
+		registerCustomerValid(customerOneId, sessionOneId);
+		registerCustomerValid(customerOneId, sessionTwoId);
+		registerCustomerValid(customerOneId, sessionThreeId);
+		registerCustomerValid(customerTwoId, sessionOneId);
+		ResponseEntity<RegistrationListDto> response = client.getForEntity("/registrations/customers/" + Integer.toString(customerOneId), 
+				RegistrationListDto.class);
+		RegistrationListDto responseBody = response.getBody();
+		List<RegistrationResponseDto> registrationDtos = responseBody.getRegistrations();
+		
+		assertNotNull(response);
+		assertNotNull(responseBody);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(registrationDtos.size(), 3);
+		assertTrue(containsRegistration(sessionOneId, registrationDtos));
+		assertTrue(containsRegistration(sessionTwoId, registrationDtos));
+		assertTrue(containsRegistration(sessionThreeId, registrationDtos));
+	}
+	
+	private void registerCustomerValid(int customerId, int sessionId) {
+		ResponseEntity<RegistrationResponseDto> response = client.exchange("/registrations/" + Integer.toString(customerId) + "/" + Integer.toString(sessionId), 
 				HttpMethod.PUT, null, RegistrationResponseDto.class);
 		assertNotNull(response);
 		assertNotNull(response.getBody());
 		assertEquals(HttpStatus.CREATED, response.getStatusCode());
 		assertEquals(customerId, response.getBody().getCustomer().getId());
-		assertEquals(sessionOneId, response.getBody().getSession().getId());
+		assertEquals(sessionId, response.getBody().getSession().getId());
+	}
+	
+	private boolean containsRegistration(int sessionId, List<RegistrationResponseDto> registrationDtos) {
+		for (RegistrationResponseDto registration: registrationDtos) {
+			if (registration.getSession().getId() == sessionId) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
