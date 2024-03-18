7b1d1f1143179dfc6c093c57f6e081c723bc4e1d
angelaxzhu <angela.x.zhu@mail.mcgill.ca>
2024-03-18 10:54:42 -0400

    wrote integration tests - server 500 error

--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/dto/LoginDto.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/dto/LoginDto.java
@@ -4,6 +4,11 @@ public class LoginDto {
     private String email;
     private String password;
 
+    public LoginDto(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
     public String getEmail() {
         return email;
     }
--- SportCenterManager-Backend/src/test/java/ca/mcgill/ecse321/SportCenterManager/integration/LoginLogoutIntegrationTests.java
+++ SportCenterManager-Backend/src/test/java/ca/mcgill/ecse321/SportCenterManager/integration/LoginLogoutIntegrationTests.java
@@ -1,58 +1,125 @@
 package ca.mcgill.ecse321.SportCenterManager.integration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import ca.mcgill.ecse321.SportCenterManager.dao.CustomerAccountRepository;
+import ca.mcgill.ecse321.SportCenterManager.dto.ErrorDto;
+import ca.mcgill.ecse321.SportCenterManager.dto.LoginDto;
+import ca.mcgill.ecse321.SportCenterManager.model.CustomerAccount;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 
 public class LoginLogoutIntegrationTests {
+    @Autowired
+    private CustomerAccountRepository customerRepo;
+    @Autowired
+    private TestRestTemplate client;
+
+    private String customerOneName = "bob";
+    private String customerTwoName="jane";
+    private String customerOneEmail = "bob@gmail.ca";
+    private String customerTwoEmail = "jane@gmail.ca";
+    private String customerOnePass = "ecse321";
+    private String customerTwoPass = "ecse223";
+    private String invalidEmail = "jack@gmail.ca";
+    private String invalidPass= "comp250"; 
     
-    @BeforeAll
+    @BeforeEach
     public void setUpDatabase(){
         populateDatabase();
     }
 
-    @AfterAll
+    @AfterEach
     public void cleanDatabase(){
         clearDatabase();
     }
 
     private void populateDatabase(){
+        CustomerAccount customerOne = new CustomerAccount(customerOneName,customerOneEmail,customerOnePass);
 
+        CustomerAccount customerTwo = new CustomerAccount(customerTwoName, customerTwoEmail, customerTwoPass);
     }
-    private void clearDatabase(){
 
+    private void clearDatabase(){
+        customerRepo.deleteAll();
     }
 
     @Test
     public void testLoginValid(){
+        LoginDto request = new LoginDto(customerOneEmail,customerOnePass);
 
+        ResponseEntity<LoginDto> response = client.postForEntity("/login", request, LoginDto.class);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        LoginDto logged_in_account = response.getBody();
+        assertNotNull(logged_in_account);
+        assertEquals(customerOneEmail,logged_in_account.getEmail());
+        assertEquals(customerOnePass,logged_in_account.getPassword());
     }
     
     @Test
-    public void testLoginInvalidUsername(){
+    public void testLoginInvalidEmail(){
+        
+        LoginDto request = new LoginDto(invalidEmail,customerOnePass);
+        ResponseEntity<ErrorDto> response = client.postForEntity("/login", request, ErrorDto.class);
 
+        assertNotNull(response);
+        assertEquals(HttpStatus.BAD_REQUEST,response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals("Invalid email or password",response.getBody().getMessage());
     }
 
     @Test
     public void testLoginInvalidPassword(){
+        LoginDto request = new LoginDto(customerOnePass,invalidPass);
+        ResponseEntity<ErrorDto> response = client.postForEntity("/login", request, ErrorDto.class);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.BAD_REQUEST,response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals("Invalid email or password",response.getBody().getMessage());
 
     }
 
     @Test
     public void testLoginBothInvalid(){
+        LoginDto request = new LoginDto(invalidEmail,invalidPass);
+        ResponseEntity<ErrorDto> response = client.postForEntity("/login", request, ErrorDto.class);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.BAD_REQUEST,response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals("Invalid email or password",response.getBody().getMessage());
 
     }
 
     @Test
     public void testLogoutValid(){
+        
+        Object request = null;
+        ResponseEntity<String> response = client.postForEntity("/logout",request, String.class);
 
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+
+        String logged_out_account = response.getBody();
+        assertNotNull(logged_out_account);
+        assertEquals("logged out",logged_out_account);
     }
 
-    @Test
-    public void testLogoutInvalid(){
-
-    }
+    //no invalid logout => if not logged in, frontend won't display logout button
 }
