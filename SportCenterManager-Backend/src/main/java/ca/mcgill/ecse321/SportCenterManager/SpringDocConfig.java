69ff8c5c018c9705396a12d15804079c8a75f7b5
narikko <116518028+narikko@users.noreply.github.com>
2024-03-20 21:45:27 -0400

    Setup dependencies and config class for API documentation

---
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/SpringDocConfig.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse321.SportCenterManager;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringDocConfig {
+  @Bean
+  public OpenAPI baseOpenAPI(){
+    return new OpenAPI().info(new Info().title("Sport Center Manager").version("1.0.0").description("API Documentation"));
+  }
+}
