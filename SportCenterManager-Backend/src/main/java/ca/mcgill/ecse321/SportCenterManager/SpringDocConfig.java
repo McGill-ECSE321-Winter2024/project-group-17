7d6da5edcd577bdcfa53ef66546ff6d776de7654
narikko <116518028+narikko@users.noreply.github.com>
2024-03-20 23:57:40 -0400

    Added spring doc annotations to billing information endpoints

--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/SpringDocConfig.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/SpringDocConfig.java
@@ -1,16 +1,30 @@
 package ca.mcgill.ecse321.SportCenterManager;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.examples.Example;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.responses.ApiResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 
 @OpenAPIDefinition
 @Configuration
 public class SpringDocConfig {
   @Bean
   public OpenAPI baseOpenAPI(){
+    ApiResponse okApi = new ApiResponse().content(
+            new Content().addMediaType(MediaType.APPLICATION_JSON_VALUE,
+                    new io.swagger.v3.oas.models.media.MediaType().addExamples("default",
+                            new Example().value("{\"code\" : 200, \"Status\" : \"OK\"}")))
+    ).description("OK");
+
+    Components components = new Components();
+    components.addResponses("okApi", okApi);
+
     return new OpenAPI().info(new Info().title("Sport Center Manager").version("1.0.0").description("API Documentation"));
   }
 }
--- SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/controller/BillingInformationController.java
+++ SportCenterManager-Backend/src/main/java/ca/mcgill/ecse321/SportCenterManager/controller/BillingInformationController.java
@@ -1,5 +1,9 @@
 package ca.mcgill.ecse321.SportCenterManager.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
 @RestController
 @RequestMapping("/customerAccounts/{customerAccount_id}/billingInformation")
 public class BillingInformationController {
@@ -20,13 +27,91 @@ public class BillingInformationController {
     private BillingInformationService billingService;
 
     @GetMapping()
+    @Operation(
+            summary = "Retrieve the billing information of a specific customer.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully retrieved billing information.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(value = "{\"code\" : 200, \"Status\" : \"OK.\"}")
+                                    }
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Failed to retrieve billing information: invalid customer id.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(value = "{\"code\" : 404, \"Status\" : \"Not Found.\"}")
+                                    }
+                            )
+                    )
+
+            }
+    )
     public BillingInformationResponseDto getBillingInformation(@PathVariable int customerAccount_id) {
         BillingInformation billingInformation = billingService.getBillingInformation(customerAccount_id);
         return new BillingInformationResponseDto(billingInformation);
     }
 
     @PutMapping()
-    public BillingInformationResponseDto updateBillingInformation(@PathVariable int customerAccount_id, @RequestBody BillingInformationRequestDto billingInfo) {
+    @Operation(
+            summary = "Update the billing information of a specific customer.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully updated billing information.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(value = "{\"code\" : 200, \"Status\" : \"OK.\"}")
+                                    }
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Failed to update billing information: invalid customer id.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(value = "{\"code\" : 404, \"Status\" : \"Not Found.\"}")
+                                    }
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Failed to update billing information: invalid parameters.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(value = "{\"code\" : 403, \"Status\" : \"Forbidden.\"}")
+                                    }
+                            )
+                    )
+            }
+    )
+    public BillingInformationResponseDto updateBillingInformation(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = "/application/json",
+                            examples = {
+                                    @ExampleObject(value = "{" +
+                                            "\"address\" : \"updatedAddress\", " +
+                                            "\"postalCode\" : \"updatedPostalCode\", " +
+                                            "\"country\" : \"updatedCountry\", " +
+                                            "\"name\" : \"updatedPostalCode\", " +
+                                            "\"cardNumber\" : \"updatedCardNumber\", " +
+                                            "\"cvc\" : 456, " +
+                                            "\"expirationDate\" : \"2024-03-21T03:30:07.329Z\"}"
+                                    )
+                            }
+                    )
+            )
+            @PathVariable int customerAccount_id, @RequestBody BillingInformationRequestDto billingInfo) {
         BillingInformation billingInformation = billingService.updateBillingInformation(customerAccount_id, billingInfo.getAddress(), billingInfo.getPostalCode(), billingInfo.getCountry(), billingInfo.getName(), billingInfo.getCardNumber(), billingInfo.getCvc(), billingInfo.getExpirationDate());
         return new BillingInformationResponseDto(billingInformation);
     }
