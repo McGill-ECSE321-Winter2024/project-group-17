79976cc6a16c09a6a4c8fabe4e1b176bae33e766
narikko <116518028+narikko@users.noreply.github.com>
2024-04-04 23:30:25 -0400

    Implemented Create Course Page

---
+++ SportCenterManager-Frontend/src/components/Courses.vue
@@ -0,0 +1,44 @@
+<template>
+   <div>
+    <p>COURSES</p>
+    <div class="grid-container">
+      <div v-for="course in courses" :key="course.id" class="grid-item">{{ course.name }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      courses: [
+        { id: 1, name: 'Item 1' },
+        { id: 2, name: 'Item 2' },
+        { id: 3, name: 'Item 3' },
+        { id: 4, name: 'Item 4' },
+        { id: 5, name: 'Item 5' },
+        { id: 6, name: 'Item 6' }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-weight: bold;
+  font-size: 18px;
+  color: black;
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr); /* 3 columns with equal width */
+  grid-gap: 10px; /* Gap between grid items */
+}
+
+.grid-item {
+  background-color: #ffffff;
+  padding: 20px;
+  text-align: center;
+}
+</style>
--- SportCenterManager-Frontend/src/router/index.js
+++ SportCenterManager-Frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Courses from '@/components/Courses'
+import CreateCourse from '@/components/CreateCourse'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/courses',
+      name: 'Courses',
+      component: Courses
+    },
+    {
+      path: '/courses/create',
+      name: 'CreateCourse',
+      component: CreateCourse
     }
   ]
 })
