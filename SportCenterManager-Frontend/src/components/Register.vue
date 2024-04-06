911d675b4300130aaba07cd63b27fae293c65d82
eric_zhu <eric.zhu@mail.mcgill.ca>
2024-04-06 00:41:41 -0400

    Skeleton file for register page

---
+++ SportCenterManager-Frontend/src/components/Register.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="register-components">
+        <div id="registrater-header">
+            <figure class="image">
+                <p style="margin-top: 20%; margin-bottom: 20%;">REGISTRATION</p>
+            </figure>
+        </div>
+        <div id="register-body">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
