dc12095da1fa3a394c1fa05cca441bb3ed9329c3
Kaos1 <116521023+Kaos1@users.noreply.github.com>
2024-04-09 18:55:40 -0400

    Update index.js

--- SportCenterManager-Frontend/src/router/index.js
+++ SportCenterManager-Frontend/src/router/index.js
@@ -3,6 +3,11 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Courses from '@/components/Courses'
 import CreateCourse from '@/components/CreateCourse'
+import AccountView from '@/components/AccountView'
+import ViewCustomerRegistrations from '@/components/ViewCustomerRegistrations'
+import BillingInformation from '@/components/BillingInformation'
+import Register from '@/components/Register'
+import RegistrationConfirmation from '@/components/RegisterConfirmation'
 
 Vue.use(Router)
 
@@ -23,5 +28,32 @@ export default new Router({
       name: 'CreateCourse',
       component: CreateCourse
     },
+    {
+      path: '/courses/sessions/register',
+      name: 'Register',
+      component: Register,
+      props: true
+    },
+    {
+      path: '/courses/sessions/register/confirmation',
+      name: 'RegistrationConfirmation',
+      component: RegistrationConfirmation,
+      props: true
+    },
+    {
+      path: '/customerAccount',
+      name: 'AccountView',
+      component: AccountView,
+      children: [
+        {
+          path: 'billing',
+          component: BillingInformation
+        },
+        {
+          path: 'registrations',
+          component: ViewCustomerRegistrations
+        }
+      ]
+    }
   ]
 })
