f673aa0d6df641f722b18b5c456c3941a9ff514f
Kaos1 <116521023+Kaos1@users.noreply.github.com>
2024-04-09 18:48:05 -0400

    conflicts

--- SportCenterManager-Frontend/src/router/index.js
+++ SportCenterManager-Frontend/src/router/index.js
@@ -3,9 +3,6 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Courses from '@/components/Courses'
 import CreateCourse from '@/components/CreateCourse'
-import ViewCustomerRegistrations from "@/components/ViewCustomerRegistrations"
-import ViewLogin from '@/components/ViewLogin.vue';
-import ViewCreateAccount from '@/components/ViewCreateAccount.vue';
 
 Vue.use(Router)
 
@@ -26,21 +23,5 @@ export default new Router({
       name: 'CreateCourse',
       component: CreateCourse
     },
-    {
-      path: "/customerAccount/registrations",
-      name: "UserRegistrations",
-      component: ViewCustomerRegistrations
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: ViewLogin
-    },
-    {
-      path: '/login/customerAccount',
-      name: 'CreateAccount',
-      component: ViewCreateAccount
-    }
-
   ]
 })
